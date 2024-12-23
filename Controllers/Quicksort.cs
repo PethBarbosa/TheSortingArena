02dadb6e0d1369e10166587bb052074697efb3be
Patrick <patrick.barbosa@totvs.com..br>
2024-12-23 20:17:26 -0300

    QuickSort: Create files

---
+++ Controllers/Quicksort.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using TheSortingArena.Algoritmos;
+using TheSortingArena.Entidades;
+using System.Diagnostics;
+
+namespace TheSortingArena.Controllers;
+
+[Route("quicksort")]
+public class QuicksortController : ControllerBase
+{
+
+    [HttpPost]
+    public IActionResult Processar([FromBody] Conjunto listaNumerica)
+    {
+        try
+        {
+            var listaValida = Filtro.Tratamento(listaNumerica.lista ?? "");
+            var bodyRetorno = new Dictionary<string, string>();
+            var temporizador = new Stopwatch();
+
+            if (listaNumerica.TipoAlgoritmo == TipoAlgoritmo.BubbleSort)
+            {
+                var conjunto = new BubbleSort(listaValida){};
+                temporizador.Start();
+                var listaOrdenada = conjunto.OrderBy();
+                temporizador.Stop();
+
+                bodyRetorno.Add("conjunto", string.Join(",", listaOrdenada));
+                bodyRetorno.Add("tempoMs", $"{ temporizador.ElapsedMilliseconds} ms");
+                bodyRetorno.Add("tempoTicks", $"{temporizador.ElapsedTicks} ticks");
+
+                return Ok(bodyRetorno);
+            }
+
+            return StatusCode(200);
+        
+        }catch
+        {
+            return BadRequest();
+        }
+    }
+}
